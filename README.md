5c3ccfcffeba16aae9f110020aac2ac0de52e42b
Simonas Karuzas <simonas@not.cat>
2022-02-02 13:33:35 +0200

    refactor

---
+++ README.md
@@ -0,0 +1,7 @@
+## Setup
+
+### Verification agent dependencies
+
+```bash
+yarn add @veramo/core @veramo/did-resolver did-resolver did-resolver ethr-did-resolver web-did-resolver @veramo/message-handler @veramo/did-jwt @veramo/url-handler @veramo/credential-w3c
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+require("cross-fetch/polyfill");
 import { Client, Intents } from "discord.js";
-import fetch from "cross-fetch";
+import { verifyMembers } from "./verify-members";
+
 const token = "OTM4MDMwMDQ5MDQxNDYxMjY5.YfkWjw.szMfYuJsdKMlvKCosjKkjrVKKmY";
 
 // https://stackoverflow.com/a/67799671/10571155
@@ -12,29 +14,10 @@ const client = new Client({
   ],
 });
 
+client.on("message", verifyMembers);
+
 client.once("ready", () => {
   console.log("Ready!");
 });
 
-client.on("message", async (message) => {
-  if (message.author.bot) return;
-
-	message.attachments.map( async attachment => {
-    // const contents = await (await fetch(attachment.url)).text();
-    // console.log(contents);
-    // verify
-		const role = message.guild?.roles.cache.find(r => r.name === "member");
-		if (role) {
-			await message.member?.roles.add(
-				role,
-				"For proving their membership with a VC issued by: Collab.land"
-			);
-			message.reply(
-				`User ${message.member?.displayName} received ${role.name} role`
-			);
-		}
-
-	})
-
-});
 client.login(token);
